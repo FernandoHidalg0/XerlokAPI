d48d9a20a84daf0ea0a6a06c98fab2e9a86ccc0a
jarbasai <jarbasai@mailfence.com>
2017-11-15 03:05:17 +0000

    pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='xerlok_api',
+    version='0.1',
+    packages=['xerlok_api'],
+    url='https://github.com/JarbasAI/XerlokAPI',
+    license='MIT',
+    author='user',
+    author_email='jarbasai@mailfence.com',
+    download_url='https://github.com/JarbasAI/XerlokAPI/archive/0.1.tar.gz',
+    keywords=['xerlok', 'api'],
+    description='xerlokAI services api'
+)
