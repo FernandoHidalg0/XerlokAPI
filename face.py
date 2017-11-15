6a124377f4c78a8f7320cd52a2e5422361d4de7e
jarbasai <jarbasai@mailfence.com>
2017-11-15 00:53:31 +0000

    up

--- face.py
+++ face.py
@@ -4,10 +4,30 @@ from requests.exceptions import ConnectionError
 
 class XerlokFace(object):
     FACE_URL = "http://159.203.78.247:5666/face_recognition/"
+    EMO_FACE_URL = "http://159.203.78.247:5667/face_recognition/emotion/"
 
     def __init__(self, api):
         self.api = api
         self.headers = {"Authorization": str(self.api)}
+
+    def recognize_emotion(self, jpg_file):
+        filepath = jpg_file
+        with open(filepath) as fh:
+            mydata = fh.read()
+        try:
+            response = requests.put(
+                XerlokFace.EMO_FACE_URL + "recognize",
+                data=mydata,
+                headers=self.headers
+            )
+        except ConnectionError:
+            raise ConnectionError("The Face Emotion Recognition service is "
+                                  "unavailable.")
+
+        try:
+            return response.json()
+        except:
+            return response.text
 
     def recognize_face(self, jpg_file):
         filepath = jpg_file
